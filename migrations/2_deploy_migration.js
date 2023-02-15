f08cf323c92bc33d027b092342ef5c213ce06054
wannagetgud <uraveragebud@gmail.com>
2023-02-15 14:51:39 +0700

    new contract

--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -4,5 +4,5 @@ const PStoreNFT = artifacts.require('PStoreNFT')
 module.exports = async (deployer) => {
   const accounts = await web3.eth.getAccounts()
 
-  await deployer.deploy(PStoreNFT, 'PStore NFT', 'PNFT', 10, accounts[1])
+  await deployer.deploy(PStoreNFT, 'PStore NFT', 'PNFT', 10, '0x583E6b3648beB26A9EA628A3fF860a636dC2c838')
 }
