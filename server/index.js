7771c0f3286ee472f6dc59676cc943efccb7c230
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-12-01 14:02:55 +0700

    fix: add patch method to cors origin

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
 app.use(cors(corsOptions))
 
